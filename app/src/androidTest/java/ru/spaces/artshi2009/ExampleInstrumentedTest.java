72cc5557bc2ecdd2d0d871f56eb5a347fa8fc687
Selefk <artshi2009@yandex.ru>
2019-04-22 16:57:50 +0300

    added code for lecture 1

---
+++ app/src/androidTest/java/ru/spaces/artshi2009/ExampleInstrumentedTest.java
@@ -0,0 +1,27 @@
+package ru.spaces.artshi2009;
+
+import android.content.Context;
+
+import androidx.test.InstrumentationRegistry;
+import androidx.test.runner.AndroidJUnit4;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static org.junit.Assert.*;
+
+/**
+ * Instrumented test, which will execute on an Android device.
+ *
+ * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
+ */
+@RunWith(AndroidJUnit4.class)
+public class ExampleInstrumentedTest {
+    @Test
+    public void useAppContext() {
+        // Context of the app under test.
+        Context appContext = InstrumentationRegistry.getTargetContext();
+
+        assertEquals("ru.spaces.artshi2009", appContext.getPackageName());
+    }
+}
